4de03955331faa17510615aa9b22e93e7d144450
Sainath Chaudhari <chaudharisainath84@gmail.com>
2026-09-06 00:52:37 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed!!", err);
+  });
